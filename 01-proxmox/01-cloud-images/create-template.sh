f7741100e5388160a2b97b6f2d506ad01763a554
Nate Trimble <904927+ngtrimble@users.noreply.github.com>
2025-11-24 16:17:21 -0800

    create-template.sh functional in current state. Requires manual steps, see output at the end of the script.

--- 01-proxmox/01-cloud-images/create-template.sh
+++ 01-proxmox/01-cloud-images/create-template.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex -o pipefail
+set -e -o pipefail
 
 # Credit to https://www.apalrd.net/posts/2023/pve_cloud/ for initial script
 
@@ -26,7 +26,7 @@ function create_template() {
     qm set $1 --memory 4096 --cores 2 --cpu host
 
     # Create drive and import downloaded image to volume
-    qm set $1 --scsi0 ${storage}:0,import-from="$(pwd)/$3",discard=on
+    qm set $1 --scsi0 ${disk_storage}:0,import-from="$(pwd)/$3",discard=on
 
     # Set scsi hardware as default boot disk using virtio-scsi-single
     qm set $1 --boot "order=scsi0;net0" --scsihw virtio-scsi-single
@@ -35,7 +35,7 @@ function create_template() {
     qm set $1 --agent enabled=1,fstrim_cloned_disks=1
 
     # Add cloud-init device
-    qm set $1 --ide0 ${storage}:cloudinit
+    qm set $1 --cdrom ${disk_storage}:cloudinit
 
     # Set CI ip config
     # IP6 = auto means SLAAC (a reliable default with no bad effects on non-IPv6 networks)
@@ -48,6 +48,9 @@ function create_template() {
     qm set $1 --cipassword ${cipassword}
     # Set the sshkeys for login
     qm set $1 --sshkeys ${sshkeys}
+    # # TODO - this refuses to take. Attempts to use this feature are being abandoned as of 11/24/2025
+    # # Set cicustom
+    # qm set $1 --cicustom ${cicustom}
  
     # Resize the disk to 8G, a reasonable minimum. You can expand it more later.
     # If the disk is already bigger than 8G, this will fail, and that is okay.
@@ -55,23 +58,14 @@ function create_template() {
 
     qm start $1
 
-    # timeout 20s ssh \
-    #   -o BatchMode=yes \ 
-    #   -o ConnectTimeout=10 \
-    #   -o ServerAliveInterval=5 \
-    #   -o ServerAliveCountMax=2 \
-    #   -o StrictHostKeyChecking=accept-new \
-    #   pve-user@ ''
+    #qm shutdown $1
 
     # Make it a template
-    qm template $1
+    #qm template $1
 }
 
-# Install whois for mkpasswd command
-apt-get update && apt-get install -y whois
-
 # Set content type local so that snippets can be used
-pvesm set local --content iso,backup,vztmpl,snippets
+pvesm set local --content iso,backup,vztmpl,snippets,images,rootdir,import
 
 # Create an ssh key pair if it does not already exist
 # You can modifiy or omit this if you prefer to supply and store your key in another manner.
@@ -83,58 +77,45 @@ fi
 #pve_key=$(head -n 1 ~/.ssh/pve-key.pub | tr -d '\n')
 export sshkeys=$pve_key_filename
 
-# Name of your storage
-export storage=local-lvm
+# Name of your disk_storage
+export disk_storage=local-lvm
 
-# Username to create on VM template
+# # Username to create on VM template
 export ciuser=pve-user
 
 # Password to use for pve-user
 export cipassword=changeme
 
-export install_qemu_guest_agent=$(cat << EOF
-apt-get update && \
-apt-get install -y qemu-guest-agent && \
-systemctl enable qemu-guest-agent && \
-systemctl start qemu-guest-agent
-EOF
-)
-
-#export password=$(mkpasswd changeme)
-
+# # TODO - this refuses to take. Attempts to use this feature are being abandoned as of 11/24/2025
 # # Cloud Init custom snippet for Proxmox / Qemu support
 # # See https://registry.terraform.io/providers/Telmate/proxmox/latest/docs/guides/cloud-init%2520getting%2520started#creating-a-snippet
 # mkdir -p /var/lib/vz/snippets
-# tee /var/lib/vz/snippets/user-data << EOF
-# network:
-#   version: 2
-#   ethernets:
-#     all-eth:
-#       match:
-#         name: "e*"
-#       dhcp4: true
-#       dhcp6: true
-#       optional: true
-      
+# tee /var/lib/vz/snippets/userdata.yaml << EOF
+# #cloud-config
+# hostname: ubuntu-24-lts-cloudimg-template
+# manage_etc_hosts: true
+# fqdn: ubuntu-24-lts-cloudimg-template
+# user: pve-user
+# password: $5$XjvKEvrR$8FursN9.LHS.0IJUft5xLba2WTKZqkBSRNpGtfQ.5H.
+# ssh_authorized_keys:
+#   - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJPP/lSyl+XTftpUdAA31KqGL6edLnAQSB2GS3sr8WRp root@obsidian
+# chpasswd:
+#   expire: False
 # users:
-#   - name: pve-user
-#     shell: /bin/bash
-#     sudo: ALL=(ALL) NOPASSWD:ALL
-#     ssh_authorized_keys:
-#       - ${pve_key}
-#     groups: users,wheel
-
+#   - default
+# package_upgrade: false
 # packages:
 #   - qemu-guest-agent
-
-# package_upgrade: true
-
 # runcmd:
 #   - systemctl enable qemu-guest-agent
 #   - systemctl start qemu-guest-agent
 # EOF
-# export cicustom="user=local:snippets/user-data"
+# export cicustom="user=local:snippets/userdata.yaml"
 
 # Ubuntu 24.04 (Noble Numbat) LTS
 curl -skL -O -C - "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
 create_template 1000 "ubuntu-24-lts-cloudimg-template" "ubuntu-24.04-server-cloudimg-amd64.img"
+
+printf "VM 1000 starting up. Log into VM and run:\n\n\t"
+printf "sudo apt-get install -y qemu-guest-agent && sudo systemctl enable qemu-guest-agent && sudo systemctl start qemu-guest-agent && sudo shutdown -h now\n"
+printf "Next make VM 1000 a template via the Proxmox UI or by issuing command: qm template 1000\n"
