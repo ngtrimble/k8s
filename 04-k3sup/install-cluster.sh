5b6704bdc175f474c420114cb95e6bf31c21cf2d
Nate Trimble <904927+ngtrimble@users.noreply.github.com>
2026-03-27 08:53:28 -0700

    Add uninstall script

--- 04-k3sup/install-cluster.sh
+++ 04-k3sup/install-cluster.sh
@@ -2,11 +2,11 @@
 
 source .env
 
-k3sup install --ip $K3S_SERVER_NODE_IP --user pveuser --ssh-key $SSH_KEY_PATH --local-path $KUBECONFIG
+k3sup install --ip $K3S_SERVER_NODE_IP --user pveuser --ssh-key $SSH_KEY_PATH --local-path $KUBECONFIG --k3s-extra-args "--disable servicelb"
 kubectl config use-context default
 
 for AGENT_NODE_IP in "${K3S_SERVER_AGENT_NODES_IPS[@]}"; do
-  k3sup join --ip $AGENT_NODE_IP --server-ip $K3S_SERVER_NODE_IP --user pveuser --ssh-key $SSH_KEY_PATH
+  k3sup join --ip $AGENT_NODE_IP --server-ip $K3S_SERVER_NODE_IP --user pveuser --ssh-key $SSH_KEY_PATH --k3s-extra-args "--disable servicelb"
 done
 
 kubectl get nodes --kubeconfig $KUBECONFIG
---
+++ 04-k3sup/uninstall-k3s.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
+source .env
+
+ssh -i $SSH_KEY_PATH pveuser@$K3S_SERVER_NODE_IP "sudo /usr/local/bin/k3s-uninstall.sh"
+
+for AGENT_NODE_IP in "${K3S_SERVER_AGENT_NODES_IPS[@]}"; do
+  ssh -i $SSH_KEY_PATH pveuser@$AGENT_NODE_IP "sudo /usr/local/bin/k3s-agent-uninstall.sh"
+done
