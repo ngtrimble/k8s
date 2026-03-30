6355d2ad5962d7d92ca5c56eeb1d95ca233b8c96
Nate Trimble <904927+ngtrimble@users.noreply.github.com>
2026-03-30 15:55:19 -0700

    updates

--- 04-k3sup/install.sh
+++ 04-k3sup/install.sh
@@ -13,16 +13,26 @@ fi
 export ENV=$1
 source env/$ENV.env
 
+export KUBECONFIG=$(pwd)/$ENV-kubeconfig
+
+# TODO - remove this code 
+rm ~/.ssh/known_hosts
+
 # Install kube-vip before joining agent nodes to ensure the VIP is available for the agent nodes to join the cluster
 pushd kube-vip
 ./install.sh $ENV
 popd
 
-k3sup install --ip $K3S_SERVER_NODE_IP --user pveuser --ssh-key $SSH_KEY_PATH --k3s-extra-args "--disable servicelb"
+k3sup install --ip $K3S_SERVER_NODE_IP --tls-san $VIP --user pveuser --ssh-key $SSH_KEY_PATH --k3s-extra-args "--disable servicelb" --local-path $KUBECONFIG
 kubectl config use-context default
+
+kubectl wait --namespace=kube-system --for=condition=Ready pods --all --timeout=300s
+kubectl rollout status -n kube-system --timeout 300s ds/kube-vip-ds 
 
 for AGENT_NODE_IP in "${K3S_SERVER_AGENT_NODES_IPS[@]}"; do
   k3sup join --ip $AGENT_NODE_IP --server-ip $VIP --user pveuser --ssh-key $SSH_KEY_PATH --k3s-extra-args "--disable servicelb"
 done
 
-kubectl get nodes --kubeconfig $KUBECONFIG
+kubectl get nodes
+
+echo -e "Set your KUBECONFIG environment variable: \n\n\texport KUBECONFIG=$(pwd)/$ENV-kubeconfig\n"
--- 04-k3sup/kube-vip/install.sh
+++ 04-k3sup/kube-vip/install.sh
@@ -12,13 +12,10 @@ if [ -z "$1" ]; then
 fi
 export ENV=$1
 source env/$ENV.env
+GENERATED_KUBE_VIP=generated-kube-vip.yaml
 
-GENERATED_KUBE_VIP="base/generated-kube-vip.yaml"
-
-curl -sL https://kube-vip.io/manifests/rbac.yaml >> $GENERATED_KUBE_VIP
-echo "---" >> $GENERATED_KUBE_VIP
-curl -sL https://raw.githubusercontent.com/kube-vip/kube-vip-cloud-provider/main/manifest/kube-vip-cloud-controller.yaml >> $GENERATED_KUBE_VIP
-echo "---" >> $GENERATED_KUBE_VIP
+curl -sL -o base/rbac.yaml https://kube-vip.io/manifests/rbac.yaml
+curl -sL -o base/kube-vip-cloud-controller.yaml https://raw.githubusercontent.com/kube-vip/kube-vip-cloud-provider/main/manifest/kube-vip-cloud-controller.yaml
 
 KVVERSION=$(curl -sL https://api.github.com/repos/kube-vip/kube-vip/releases | jq -r ".[0].name")
 alias kube-vip="nerdctl run --network host --rm ghcr.io/kube-vip/kube-vip:$KVVERSION"
@@ -29,7 +26,13 @@ kube-vip manifest daemonset \
     --inCluster \
     --taint \
     --controlplane \
-    --services | tee -a $GENERATED_KUBE_VIP
+    --services \
+    --arp \
+    --leaderElection | tee base/kube-vip.yaml
+
+pushd overlays/$ENV
+kustomize build . > ../../$GENERATED_KUBE_VIP
+popd
 
 scp -i $SSH_KEY_PATH $GENERATED_KUBE_VIP pveuser@$K3S_SERVER_NODE_IP:/tmp/kube-vip.yaml
 
